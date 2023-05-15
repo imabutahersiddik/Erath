516cfbcf9142feb232e6c2a92235e163324f4927
ki-ask <site@kiask.xyz>
2023-05-16 01:13:02 +0600

    Updating Web App

--- api/index.php
+++ api/index.php
@@ -18,9 +18,7 @@
     <link href="http://getbootstrap.com/2.3.2/assets/js/google-code-prettify/prettify.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/gh/jschr/bootstrap-modal@master/css/bootstrap-modal.css" rel="stylesheet" />
     <script
-            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
-            crossorigin="anonymous"></script>
+            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
