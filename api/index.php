b17107d9b7b6deb4eef7695a85c8ae4d3f4d9841
ki-ask <site@kiask.xyz>
2023-05-16 01:23:36 +0600

    Updating Web App

--- api/index.php
+++ api/index.php
@@ -21,7 +21,6 @@
     <script
             src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/mladenplavsic/bootstrap-navbar-sidebar@master/docs/docs.js"></script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
