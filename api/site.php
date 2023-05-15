ef8679d86496dbbbec7cdb298f318b419288ccb9
ki-ask <site@kiask.xyz>
2023-05-15 21:22:16 +0600

    Updating Web App

--- api/site.php
+++ api/site.php
@@ -21,35 +21,31 @@
 
 <script>
 	function encrypt() {
-		var htmlCode = document.getElementById('html-code').value;
-		var encrypted = CryptoJS.AES.encrypt(htmlCode, "SecretKey123");
+    var htmlCode = document.getElementById('html-code').value;
+    var encrypted = CryptoJS.AES.encrypt(htmlCode, "SecretKey123");
 
-		// Generating URL with querystring containing the encrypted data
-		var urlParams = new URLSearchParams(window.location.search);
-		urlParams.set('encrypted-html', encrypted);
-		var url = window.location.origin + window.location.pathname + '?' + urlParams.toString();
+    // Displaying encrypted URL to user
+    var url = window.location.origin + window.location.pathname + '?encrypted-html=' + encodeURIComponent(encrypted);
+    document.getElementById('encrypted-url').innerHTML = '<p>Your encrypted URL:</p><pre>' + url + '</pre>';
 
-		// Displaying encrypted URL to user
-		document.getElementById('encrypted-url').innerHTML = '<p>Your encrypted URL:</p><pre>' + url + '</pre>';
+    // Clear existing HTML from decrypted-html div
+    document.getElementById('decrypted-html').innerHTML = '';
 
-		// Clear existing HTML from decrypted-html div
-		document.getElementById('decrypted-html').innerHTML = '';
+    // Append a div element to the decrypted-html div
+    var div = document.createElement('div');
+    div.innerHTML = htmlCode;
+    document.getElementById('decrypted-html').appendChild(div);
 
-		// Append a div element to the decrypted-html div
-		var div = document.createElement('div');
-		div.innerHTML = htmlCode;
-		document.getElementById('decrypted-html').appendChild(div);
+    // Hide the form fields
+    document.getElementById('html-code').style.display = 'none';
+    document.querySelector('form button:first-of-type').style.display = 'none';
+    document.querySelector('form button:last-of-type').style.display = 'inline-block';
 
-		// Hide the form fields
-			document.getElementById('html-code').style.display = 'none';
-	document.querySelector('form button:first-of-type').style.display = 'none';
-	document.querySelector('form button:last-of-type').style.display = 'inline-block';
-
-	// Update the URL with the encrypted HTML using the pushState method
-	if (history.pushState) {
-		var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?encrypted-html=' + encodeURIComponent(encrypted);
-		window.history.pushState({path: newUrl}, '', newUrl);
-	}
+    // Update the URL with the encrypted HTML using the pushState method
+    if (history.pushState) {
+        var newUrl = url;
+        window.history.pushState({path: newUrl}, '', newUrl);
+    }
 }
 
 function decrypt() {
@@ -61,24 +57,35 @@ function decrypt() {
         var decrypted = CryptoJS.AES.decrypt(encryptedHtml, "SecretKey123");
         var html = decrypted.toString(CryptoJS.enc.Utf8);
         document.getElementById('decrypted-html').innerHTML = html;
+
+        // Hide the form fields
+        document.getElementById('html-code').style.display = 'none';
+        document.querySelector('form button:first-of-type').style.display = 'none';
+        document.querySelector('form button:last-of-type').style.display = 'none';
+    } else {
+        // Show the form fields
+        document.getElementById('html-code').style.display = 'block';
+        document.querySelector('form button:first-of-type').style.display = 'inline-block';
+               document.querySelector('form button:last-of-type').style.display = 'none';
     }
 }
-
 
 function copyURL() {
-	var encryptedURL = document.getElementById('encrypted-url').getElementsByTagName('pre')[0].textContent;
-	navigator.clipboard.writeText(encryptedURL);
-alert('URL copied to clipboard!');
+    var encryptedURL = document.getElementById('encrypted-url').getElementsByTagName('pre')[0].textContent;
+    navigator.clipboard.writeText(encryptedURL);
+    alert('URL copied to clipboard!');
 }
+
 // Call the decrypt function on page load
-if (window.location.search.includes('encrypted-html')) {
-    decrypt();
+decrypt();
+
+// Hide the form fields if data is available in the URL
+var pathParts = window.location.pathname.split('/');
+if (pathParts[1] === 'site' && pathParts[2]) {
+    document.getElementById('html-code').style.display = 'none';
+    document.querySelector('form button:first-of-type').style.display = 'none';
+    document.querySelector('form button:last-of-type').style.display = 'none';
 } else {
-    var pathParts = window.location.pathname.split('/');
-    if (pathParts[1] === 'site' && pathParts[2]) {
-        document.querySelector('form button:first-of-type').style.display = 'none';
-    } else {
-        document.querySelector('form button:first-of-type').style.display = 'inline-block';
-    }
+    document.querySelector('form button:first-of-type').style.display = 'inline-block';
 }
 </script> <div id="encrypted-url"></div> <div id="decrypted-html"></div> </body> </html>
