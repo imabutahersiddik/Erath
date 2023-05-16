67ac2e4c3b2d7c94c8ac8b96761ad74982b8342d
ki-ask <site@kiask.xyz>
2023-05-16 17:02:52 +0600

    Updating Web App

--- api/index.php
+++ api/index.php
@@ -39,9 +39,8 @@
     background-color: #fff !important;
     background-color: rgb(255 255 255/var(--tw-bg-opacity)) !important;
 }
-  .navbar-toggler-icon {
-background-image: url('data
-/svg+xml;utf8,<svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><path stroke="black" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"/></svg>');
+  .navbar-dark .navbar-toggler-icon {
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgba(17, 24, 39, 0.8)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e") !important;
 }
  </style>
 </head>
