1d67b57ce49968caf8522430368c1b62a80a65ff
ki-ask <site@kiask.xyz>
2023-05-16 19:36:27 +0600

    Updating Web App

--- api/index.php
+++ api/index.php
@@ -90,7 +90,7 @@
 <div class="container">
  <form onsubmit="return false;">
         <label for="html-code">Enter your HTML code:</label><br>
-        <textarea id="html-code" name="html-code" rows="10" cols="50"></textarea><br><br>
+        <textarea id="html-code" name="html-code"></textarea><br><br>
         <button type="button" onclick="encrypt()">Generate Encrypted URL</button>
         <button type="button" onclick="copyURL()">Copy URL to Clipboard</button>
     </form>
