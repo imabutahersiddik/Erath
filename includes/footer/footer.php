5ba0b2be19cc753c767f49ad48125a6f5c19e85a
ki-ask <site@kiask.xyz>
2023-05-27 03:25:30 +0600

    Updating Web App

--- includes/footer/footer.php
+++ includes/footer/footer.php
@@ -39,10 +39,14 @@
      </div>
    </div>
 </div>
-    <script type="text/javascript" id="cookiebanner"
-    src="https://cdn.jsdelivr.net/gh/dobarkod/cookie-banner@1.2.2/dist/cookiebanner.min.js"
-    data-height="20px" data-position="top"
-    data-message="We use cookies to improve your browsing experience.">
+    <script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.8/src/glowCookies.min.js"></script>
+   <script>
+    glowCookies.start('en', {
+        style: 1,
+        analytics: '',
+        facebookPixel: '',
+        policyLink: 'https://commission.europa.eu/cookies-policy_en'
+    });
    </script>
    <script src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/js/kwuwtk.js"></script>
    <script src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/js/system.js"></script>
