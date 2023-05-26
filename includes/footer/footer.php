67bfaaaa13b75fc6aaf77b84e3d1ef519a483326
ki-ask <site@kiask.xyz>
2023-05-27 02:10:18 +0600

    Updating Web App

--- includes/footer/footer.php
+++ includes/footer/footer.php
@@ -28,9 +28,13 @@
         </div>
       </div>
     </div>
-  </div>
-  <div class="footerpicsay"></div>
+  </div>  
 </footer>
+<div class="container">
+   <div class="row">
+      <div class="footerpicsay"></div>
+   </div>
+</div>
    <script src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/js/kwuwtk.js"></script>
    <script src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/js/system.js"></script>
    <script src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/js/theme.js"></script>
