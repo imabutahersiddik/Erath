9709f90fef15c66473ffbdeb0224b88040b31f2a
ki-ask <site@kiask.xyz>
2023-05-26 15:26:06 +0600

    Updating...

--- api/index.php
+++ api/index.php
@@ -478,7 +478,31 @@
              document.querySelector('form button:first-of-type').style.display = 'inline-block';
          }
       </script>
-      <script>
+        <script>
+    // Load saved theme and sidebar position from cookies
+    function loadSettings() {
+      var cookies = document.cookie.split("; ");
+      for (var i = 0; i < cookies.length; i++) {
+        var keyValue = cookies[i].split("=");
+        if (keyValue[0] === "theme") {
+          document.getElementById("theme_select").value = keyValue[1];
+          selectTheme(keyValue[1]);
+        }
+        if (keyValue[0] === "sidebarPosition") {
+          var buttons = document.querySelectorAll('.btn-group > button');
+          for (var j = 0; j < buttons.length; j++) {
+            if (buttons[j].getAttribute('data-class') === keyValue[1]) {
+              buttons[j].classList.add('active');
+            } else {
+              buttons[j].classList.remove('active');
+            }
+          }
+          document.body.className = keyValue[1];
+        }
+      }
+    }
+
+    // Save selected theme and sidebar position as cookies
     function saveSettings() {
       // Get the selected value of the theme_select element
       var theme = document.getElementById("theme_select").value;
@@ -490,6 +514,9 @@
       // Save the theme value as a cookie
       document.cookie = "theme=" + theme + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
     }
+
+    // Call the loadSettings function when the page is loaded
+    window.onload = loadSettings;
   </script>
    </body>
 </html>
