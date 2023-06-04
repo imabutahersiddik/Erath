6d308a6964eb4672f6b5697d1f478373ce6b0775
ki-ask <site@kiask.xyz>
2023-06-04 12:56:54 +0600

    Updating...

--- includes/header/styles.php
+++ includes/header/styles.php
@@ -1,3 +1,3 @@
 <style>
-.plugin-list{padding:0.4rem;border-radius:.4rem;text-align:center;order:1px solid var(--accents-2);box-shadow:4px 4px 4px 4px #fafafa;background:var(--accents-1)}.plugin-list img{width:100%;border-radius:2px 2px 0 0}.plugin-title{padding:0.4rem;font-wieght:700;font-size:24px}.plugin-title a{color:#444;font-weight:700;text-decoration:none;font-size:19px}
+.plugin-list{padding:0.4rem;border-radius:.4rem;text-align:center;order:1px solid var(--accents-2);box-shadow:4px 4px 4px 4px #fafafa;background:var(--accents-1)}.plugin-list img{width:100%;border-radius:2px 2px 0 0}.plugin-title{margin:0px auto;padding:0.4rem;font-weight:700;font-size:24px}.plugin-title a{color:#444;font-weight:700;text-decoration:none;font-size:19px}
 </style>
