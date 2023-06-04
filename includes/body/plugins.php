bec4d921ca9e2e00d94dd3dd940e68a6d38a7b11
ki-ask <site@kiask.xyz>
2023-06-04 13:15:05 +0600

    Updating...

--- includes/body/plugins.php
+++ includes/body/plugins.php
@@ -2,7 +2,7 @@
 <div class="row">
 <div class="col-md-3">
 <div class="plugin-list">
-<img src="https://ph-files.imgix.net/9f55c156-9421-42f7-b0be-7e1fd8151847.png" alt="Decentrealized Blog">
+<img src="https://cdn.jsdelivr.net/gh/ki-ask/decentrealizedweb@main/public/img/go-decentrealized-blog.png" alt="Decentrealized Blog">
 <div class="plugin-title">
 <a href="https://blog.decentrealizedweb.xyz">Decentrealized Blog</a>
 </div>
