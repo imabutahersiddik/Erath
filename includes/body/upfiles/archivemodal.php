d7f93c4999f7bef6e897fefce015793d9e6643d5
imabutahersiddik <abutahersiddik313@gmail.com>
2024-08-10 13:32:36 +0600

    Fixed id on archive and document modal

--- includes/body/upfiles/archivemodal.php
+++ includes/body/upfiles/archivemodal.php
@@ -1,4 +1,4 @@
-         <div class="modal fade" id="pdf">
+         <div class="modal fade" id="archivemodal">
             <div class="modal-dialog">
                <div class="modal-content">
                   <div class="modal-header">
