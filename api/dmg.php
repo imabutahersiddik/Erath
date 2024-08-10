b5164b36e96cd09412cbd8af8a09fea0edd6f472
imabutahersiddik <abutahersiddik313@gmail.com>
2024-08-10 19:46:11 +0600

    Fixed Bugs

--- api/dmg.php
+++ api/dmg.php
@@ -123,7 +123,7 @@
             }
 
             // Prepare the full Base64 URL
-            const base64Url = `data:application/dmg;base64,${encryptedData}`;
+            const base64Url = `data:application/x-apple-diskimage;base64,${encryptedData}`;
 
             // Show the output URL
             const outputUrl = document.getElementById('outputUrl');
--- api/txt.php
+++ api/txt.php
@@ -95,7 +95,7 @@
             }
 
             // Prepare the full Base64 URL
-            const base64Url = `data:application/txt;base64,${encryptedData}`;
+            const base64Url = `data:text/plain;base64,${encryptedData}`;
 
             // Show the output URL
             const outputUrl = document.getElementById('outputUrl');
