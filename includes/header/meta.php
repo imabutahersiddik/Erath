e252d9785cb3c4fd2072c1c7fbb82b299dc274e2
imabutahersiddik <abutahersiddik313@gmail.com>
2024-08-11 09:39:45 +0600

    Updated Title and Description

--- includes/header/meta.php
+++ includes/header/meta.php
@@ -1,8 +1,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Erath - Storage-Less Web Page Hosting</title>
-<meta name="description" content="Discover Erath Hosting, the revolutionary storage-less web page hosting service where your data is stored locally on your computer. Enjoy fast, secure, and efficient web hosting without relying on server storage.">
-<meta name="keywords" content="Erath Hosting, storage-less web hosting, web page hosting, local data storage, decentralized hosting, secure web hosting, efficient hosting solutions">
+<title>Erath - Storage-Less Free Web Page Hosting</title>
+<meta name="description" content="Discover Erath Hosting, the revolutionary storage-less free web page hosting service where your data is stored locally on your computer. Enjoy fast, secure, and efficient web hosting without relying on server storage.">
+<meta name="keywords" content="Erath Hosting, storage-less web hosting,free web page hosting, web page hosting, local data storage, decentralized hosting, secure web hosting, efficient hosting solutions">
  <link rel="icon" type="image/png" href="https://cdn.jsdelivr.net/gh/imabutahersiddik/decentralized-web@main/public/icons/decentrealized-web-coming-out-from-night-to-shine-the-day-picsay.png">    
+ <meta name="google-site-verification" content="zy0cnBId0sSwOCYm6vzPYie85QFFxRQoe0RIzxQBGU8" />
 
 
